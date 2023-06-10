7d2d1b6b5d2afee1840d57479b2d3cec6557325b
eric <erotush77@gmail.com>
2023-06-10 03:34:51 +0300

    Added custom Select component

---
+++ src/components/formComponents/Select.js
@@ -0,0 +1,17 @@
+
+
+export default function Select(props) {
+
+    return (
+        <select
+          ref={props.selectref}
+          {...props}
+          className='flex-none w-full p-2 border-2 border-gray-200 bg-[#f9fafb] rounded flex-grow  placeholder-gray-100 focus:bg-white focus:border-gray-600 outline-none'>
+          {
+            ((_options) => _options.map(({ value, label }, i) =>
+              <option className='py-1 hover:bg-red-300 text-normal font-normal' key={i} value={value}>{label}</option>
+            ))(props.options?.length ? props.options : [])
+          }
+        </select>
+    )
+}
